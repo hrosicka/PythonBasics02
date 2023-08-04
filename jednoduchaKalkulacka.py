d10aded43e8693fff8d98e0b229b4ca71d2e25a5
Hanka <hrosicka@post.cz>
2023-08-04 18:33:37 +0200

    Úprava jednoduché kalkulačky

--- jednoduchaKalkulacka.py
+++ jednoduchaKalkulacka.py
@@ -4,20 +4,28 @@ print("---------------------")
 prvniCislo = float(input("Zadejte první číslo: "))
 druheCislo = float(input("Zadejte druhé číslo: "))
 
-soucet = prvniCislo - druheCislo
+soucet = prvniCislo + druheCislo
 rozdil = prvniCislo - druheCislo
 soucin = prvniCislo * druheCislo
 
 
-print('%.3f + %.3f = %.3f' % (prvniCislo, druheCislo, soucet))
-print('%.3f - %.3f = %.3f' % (prvniCislo, druheCislo, rozdil))
-print('%.3f * %.3f = %.3f' % (prvniCislo, druheCislo, soucin))
+print('Součet:\t\t\t%.3f + %.3f = %.3f' % (prvniCislo, druheCislo, soucet))
+print('Rozdíl:\t\t\t%.3f - %.3f = %.3f' % (prvniCislo, druheCislo, rozdil))
+print('Součin:\t\t\t%.3f * %.3f = %.3f' % (prvniCislo, druheCislo, soucin))
 
 
 if (druheCislo != 0):
     podil = prvniCislo / druheCislo
-    print('%.3f / %.3f = %.3f' % (prvniCislo, druheCislo, podil))
+    print('Podíl:\t\t\t%.3f / %.3f = %.3f' % (prvniCislo, druheCislo, podil))
+    celociselneDeleni = prvniCislo // druheCislo
+    print('Celočíselné dělení:\t%.3f // %.3f = %.3f' % (prvniCislo, druheCislo, celociselneDeleni))
+    zbytek = prvniCislo % druheCislo
+    print('Zbytek po dělení:\t%.3f // %.3f = %.3f' % (prvniCislo, druheCislo, zbytek))
+
     
 else:
-    print('%.3f / %.3f = ???' % (prvniCislo, druheCislo))
+    print('Podíl:\t\t\t%.3f / %.3f = ???' % (prvniCislo, druheCislo))
     print("Nulou nelze dělit!!!")
+
+umocneni = prvniCislo ** druheCislo
+print('Umocnění:\t\t%.3f ** %.3f = %.3f' % (prvniCislo, druheCislo, umocneni))
