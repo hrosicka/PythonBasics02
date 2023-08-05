fa539d8f179d29ffb8d87b538dd0abeb4453c769
Hanka <hrosicka@post.cz>
2023-08-05 15:01:22 +0200

    Hra kámen nůžky papír upraveno do funkcí

--- hraKamenNuzkyPapir.py
+++ hraKamenNuzkyPapir.py
@@ -2,6 +2,35 @@
 # počítač náhodně vygeneruje čísla
 from random import randrange
 
+# funkce pro tah počítače, použije se random
+def tahnulPocitac():
+    # počítač náhodně vygeneruje čísla 0, 1 nebo 2, ze kterých se potom stanoví tah
+    nahoda = randrange(0, 3)
+
+    if nahoda == 0:
+        tahPocitace = 'kámen'
+
+    elif nahoda == 1:
+        tahPocitace = 'nůžky'
+
+    else:
+        tahPocitace = 'papír'
+
+    return tahPocitace
+    
+# funkce pro tah hráče
+def tahnulHrac():
+
+    # hráč musí zadat slovně svoji volbu
+    tahHrace = input("Hraješ! Zadej buď kámen nůžky nebo papír: ")
+
+    # pokud hráč zadá překlep, tak opakuje zadání dokud ho nezadá správně
+    while ((tahHrace != 'kámen') and (tahHrace != 'nůžky') and (tahHrace != 'papír')):
+        tahHrace = input("Zadal jsi to špatně! Zadej buď kámen nůžky nebo papír: ")
+
+    return tahHrace
+
+
 def main():
 
     print("\nHRA KÁMEN NŮŽKY PAPÍR")
@@ -12,62 +41,48 @@ def main():
     countHrac = 0
     countPocitac = 0
 
+    
+
     while (konec != True):
 
-        # počítač náhodně vygeneruje čísla 0, 1 nebo 2, ze kterých se potom stanoví tah
-        nahoda = randrange(0, 3)
-
-        if nahoda == 0:
-            tahPocitace = 'kámen'
-
-        elif nahoda == 1:
-            tahPocitace = 'nůžky'
-
-        else:
-            tahPocitace = 'papír'
-        
-        # hráč musí zadat slovně svoji volbu
-        tahHrace = input("Hraješ! Zadej buď kámen nůžky nebo papír: ")
-
-        # pokud hráč zadá překlep, tak opakuje zadání dokud ho nezadá správně
-        while ((tahHrace != 'kámen') and (tahHrace != 'nůžky') and (tahHrace != 'papír')):
-            tahHrace = input("Zadal jsi to špatně! Zadej buď kámen nůžky nebo papír: ")
+        hralPocitac = tahnulPocitac()
+        hralHrac = tahnulHrac()
 
         # vyhodnocení hry
         # tah hráče kámen
-        if tahHrace == 'kámen' and tahPocitace == 'kámen':
+        if hralHrac == 'kámen' and hralPocitac == 'kámen':
             print("Remíza!!!")
         
-        elif tahHrace == 'kámen' and tahPocitace == 'nůžky':
+        elif hralHrac == 'kámen' and hralPocitac == 'nůžky':
             countHrac = countHrac + 1
             print("Vyhrál jsi!!!")
 
-        elif tahHrace == 'kámen' and tahPocitace == 'papír':
+        elif hralHrac == 'kámen' and hralPocitac == 'papír':
             countPocitac = countPocitac + 1
             print("Prohrál jsi!!!")
 
         # tah hráče nůžky
-        elif tahHrace == 'nůžky' and tahPocitace == 'kámen':
+        elif hralHrac == 'nůžky' and hralPocitac == 'kámen':
             countPocitac = countPocitac + 1
             print("Prohrál jsi!!!")
         
-        elif tahHrace == 'nůžky' and tahPocitace == 'nůžky':
+        elif hralHrac == 'nůžky' and hralPocitac == 'nůžky':
             print("Remíza!!!")
 
-        elif tahHrace == 'nůžky' and tahPocitace == 'papír':
+        elif hralHrac == 'nůžky' and hralPocitac == 'papír':
             countHrac = countHrac + 1
             print("Vyhrál jsi!!!")
 
         # tah hráče papír
-        elif tahHrace == 'papír' and tahPocitace == 'kámen':
+        elif hralHrac == 'papír' and hralPocitac == 'kámen':
             countHrac = countHrac + 1
             print("Vyhrál jsi!!!")
         
-        elif tahHrace == 'papír' and tahPocitace == 'nůžky':
+        elif hralHrac == 'papír' and hralPocitac == 'nůžky':
             countPocitac = countPocitac + 1
             print("Prohrál jsi!!!")
 
-        elif tahHrace == 'papír' and tahPocitace == 'papír':
+        elif hralHrac == 'papír' and hralPocitac == 'papír':
             print("Remíza!!!")
         
 
