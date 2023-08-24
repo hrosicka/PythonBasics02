51de0683a7f2018c379946a39d9e76fb11ec55db
Hanka <hrosicka@post.cz>
2023-08-24 20:24:18 +0200

    Doplnění komentářů do třídy čtverec

--- ctverec.py
+++ ctverec.py
@@ -1,16 +1,22 @@
+# třída čtverec
+# počítá obvod, obsah a vypisuje výsledek
 class Ctverec:
     
     def __init__(self, a):
         self.a = a
 
+    # metoda pro výpočet obvodu čtverce
     def obvod(self):
         obv = 4 * self.a
         return obv
     
+    # metoda pro výpočet obsahu čtverce
     def obsah(self):
         obs = pow(self.a, 2)
         return obs
 
+    # metoda pro vypsání obsahu a obvodu čtverce
+    # vrací 2 řetězce
     def vypis(self):
         textObvod = "Čtverec o straně {} cm má obvod {} cm.".format(self.a, self.obvod())
         textObsah = "Čtverec o straně {} cm má obsah {} cm2.".format(self.a, self.obsah())
